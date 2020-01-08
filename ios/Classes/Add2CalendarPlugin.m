e64bb5987196dec865a5c9d5ee1573ef2ec71c4b
Willy Liu <willyliu@kkbox.com>
2020-01-08 17:27:20 +0800

    Reverts to import Swift ObjC interface header without framework name.

--- ios/Classes/Add2CalendarPlugin.m
+++ ios/Classes/Add2CalendarPlugin.m
@@ -1,5 +1,7 @@
 #import "Add2CalendarPlugin.h"
-#import <add_2_calendar/add_2_calendar-Swift.h>
+#import "add_2_calendar-Swift.h"    // for built as static library
+// #import <add_2_calendar/add_2_calendar-Swift.h>   // uncomment this line for built as dynamic framework
+// Ref: https://developer.apple.com/documentation/swift/imported_c_and_objective-c_apis/importing_swift_into_objective-c
 
 @implementation Add2CalendarPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
